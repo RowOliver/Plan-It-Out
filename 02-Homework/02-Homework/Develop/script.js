b6ec5cc0a75c7c9316f42266fa4dc96042cf3eac
Rowan Oliver <r.oliver.yoga@gmail.com>
2020-11-27 13:41:36 -0800

    created JS file, trying to get time to render on page

---
+++ 02-Homework/02-Homework/Develop/script.js
@@ -0,0 +1,11 @@
+// show current time on main page
+// create time boxes for 9am-7pm allow for more boxes if needed
+// color coded system of time frames past,present and future 
+// make events saveble on local storage 
+// everything saves even when the page is refreshed 
+
+
+var currentTime = moment().currentTime;
+
+
+
