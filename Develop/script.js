5d11d42ca535efb5086ad42b65c6662edb2a7797
Rowan Oliver <r.oliver.yoga@gmail.com>
2020-12-08 15:26:59 -0800

    time display-i-2

--- Develop/script.js
+++ Develop/script.js
@@ -10,14 +10,70 @@ setInterval(function() {
 
 
 //local storage and storing appointments
-function saveAppointment(){
-  var nineBtn = document.getElementById("textarea9").nodeValue;
+function saveAppointmentNine() {
+  var nineBtn = document.getElementById("hour-6").nodeValue;
   localStorage.setItem(nineBtn, "nineBtn");
+  alert("Appointment saved");
+  console.log(localStorage);
+
+}
+
+function saveAppointmentTen() {
+  var tenBtn = document.getElementById("hour-10").nodeValue;
+  localStorage.setItem(tenBtn, "tenBtn");
   alert("Appointment saved");
   console.log(localStorage);
 }
 
 
+function saveAppointmentEleven() {
+  var elevenBtn = document.getElementById("hour-11").nodeValue;
+  localStorage.setItem(elevenBtn, "elevenBtn");
+  alert("Appointment saved");
+  console.log(localStorage);
+}
+
+function saveAppointmentT() {
+  var oneBtn = document.getElementById("hour-12").nodeValue;
+  localStorage.setItem(oneBtn, "oneBtn");
+  alert("Appointment saved");
+  console.log(localStorage);
+}
+
+function saveAppointmentThirteen() {
+  var oneBtn = document.getElementById("hour-1").nodeValue;
+  localStorage.setItem(oneBtn, "oneBtn");
+  alert("Appointment saved");
+  console.log(localStorage);
+}
+
+function saveAppointmentFourteen() {
+  var twoBtn = document.getElementById("hour-2").nodeValue;
+  localStorage.setItem(twoBtn, "twoBtn");
+  alert("Appointment saved");
+  console.log(localStorage);
+}
+
+function saveAppointmentFifteen() {
+  var threeBtn = document.getElementById("hour-3").nodeValue;
+  localStorage.setItem(threeBtn, "threeBtn");
+  alert("Appointment saved");
+  console.log(localStorage);
+}
+
+function saveAppointmentSixteen() {
+  var fourBtn = document.getElementById("hour-4").nodeValue;
+  localStorage.setItem(fourBtn, "fourBtn");
+  alert("Appointment saved");
+  console.log(localStorage);
+}
+
+function saveAppointmentSeventeen() {
+  var fiveBtn = document.getElementById("hour-5").nodeValue;
+  localStorage.setItem(fiveBtn, "fiveBtn");
+  alert("Appointment saved");
+  console.log(localStorage);
+}
 
 
 
