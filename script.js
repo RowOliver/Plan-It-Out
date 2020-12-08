c434f821071c56ad4949945e647968762ffb1227
Rowan Oliver <r.oliver.yoga@gmail.com>
2020-12-07 19:23:53 -0800

    made hr rows

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+//display current time 
+function currentDay(){
+  var day = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+  $("currentDay").text(day)
+}
+
+
+
 
 
 var timeBlock = document.getElementById("time-block");
 console.log(timeBlock);
+
