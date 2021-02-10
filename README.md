0fd2f33719c7ffc4987704976215379f2fb3b6b1
Rowan Oliver <r.oliver.yoga@gmail.com>
2021-02-09 21:50:52 -0800

    color coding works, moment works

--- README.md
+++ README.md
@@ -1,33 +1,14 @@
-#Weekly Planner
- 
-Weekly Planner is a planning application that allows the user to be able to set appointments during regualar office hours and see what appointments are happening in the past,present and furture with a color coding system.
+# Plan It Out 
 
 
-User Story:
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
+# Description
+Plan It Out is a planning application that allows the user to be able to set appointments during regualar office hours and see what appointments are happening in the past,present and furture with a color coding system.
 
-Installation:
-There is no installation required 
-
-
-Strategy:
-
-Using Moment JS, the current day will display as to improve the functionality of the planner 
-
-Using Moment the current date is displayed at the time of the screen 
+![planner](Develop/planner.png)
 
 
 
-I created a time container with a button that is used to save the appointment to local storage 
-
-![button](images/button-html.png)
 
 
-Then in JavaScript a function was created for each that would save the appointment 
-
-![function](images/app-function.png)
 
 
-Color change for different time periods of past,present and future 
--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 var currentDate = moment(). format("MM/DD/YYYY, h:mm:ss a");
 $("#theDate").append(currentDate);
 console.log(currentDate);
+
  
 var timeArr = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
 
+
 function updatetime() {
-  var currentTime = moment().format('H');
+  var currentTime = moment().format('h');
   for(var i=0; i < timeArr.length; i++) {
     console.log(currentTime);
-
+    //the future
     if(parseInt(timeArr[i]) > currentTime){
       $("#" + timeArr[i]).attr("style", "background-color: lightblue");
 
       
-    }
+    }//the past 
     else if (parseInt(timeArr[i]) < currentTime) {
       ("#" + timeArr[i]).attr("style", "background-color:lightpurple");
 
 
-    }
+    }//the present 
     else if (parseInt(timeArr[i]) == currentTime) {
       $("#" + timeArr[i]).attr("style", "background-color: lightgreen");
     }
   }
  
-};
+}
+  updatetime();
+
+  $(".rowBtn").on("click", function(){
+    var currentTime = $(this).parent().attr("id");
+    var dayPlan = $(this).parent().find("input").val();
+    localStorage.setItem(currentTime, dayPlan);
+
+    console.log(dayPlan);
+  });
+
+  $("#time").each(function(){
+    var currentTime = $(this).attr("input");
+    var dayPlan = localStorage.getItem(currentTime);
+    $(this).find("id").val(dayPlan);
+  });
+
+
 
 
